22a3c1a6eca6a9d8c79873c7ce883d7d55d64538
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-18 10:53:02 +0500

    besh

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' with data type int.
- 
+number=23
 #Create a variable called 'answer'
-
+answer=(number%3)
 #Find the remainder of the division of number by 3.
- 
 #Print the answer
+print(answer)
