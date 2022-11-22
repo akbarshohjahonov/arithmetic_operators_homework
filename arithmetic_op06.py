56288e4316781c3354db12cc21ef790750391fb7
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-22 19:00:07 +0500

    oxiri

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,5 @@
 #Create a variable called 'number' with data type int.
-number=47
+number=45
 #Divide the number by 2 and assign number.
 number=(number/2)
 #Find the fraction of the number and assing to a variable called 'answer'.
